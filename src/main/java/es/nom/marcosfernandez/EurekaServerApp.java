bf22f942906ce5e67c8ef56155bde0aa02b9e125
gizmodemente <gizmodemente@googlemail.com>
2018-04-29 16:04:28 +0200

    Gradle flavor

--- src/main/java/es/nom/marcosfernandez/EurekaServerApp.java
+++ src/main/java/es/nom/marcosfernandez/EurekaServerApp.java
@@ -1,16 +1,15 @@
 package es.nom.marcosfernandez;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
 
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApp {
 
     public static void main(String[] args) {
-        new SpringApplicationBuilder(EurekaServerApp.class).web(true).run(args);
+        SpringApplication.run(EurekaServerApp.class, args);
     }
 
 }
